731471527632435d759a00f37f8c1df8ce291ab9
Trieu Ho <trieu.ho@flextrade.com>
2020-02-21 17:28:31 +0000

    read in the data

--- report_generator.py
+++ report_generator.py
@@ -1,1 +1,21 @@
-print("Hello")
+import pandas as pd
+
+data_file_excel = 'data.xlsx'
+
+tables_df = pd.read_excel(data_file_excel, skiprows=1)
+
+trade_df = tables_df.iloc[:, 0:4]
+print(trade_df)
+
+instrument_df = tables_df.iloc[:, 5:9]
+print(instrument_df)
+
+
+contract_df = tables_df.iloc[:, 10:14]
+print(contract_df)
+
+eod_prices_df = tables_df.iloc[:, 15:32]
+print(eod_prices_df)
+
+fx_conversion_df = tables_df.iloc[:, 50:54]
+print(fx_conversion_df)
